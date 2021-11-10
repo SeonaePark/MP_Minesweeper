f58ab126ce6c529cfdbcccd244214f6749d091b6
seonae <sunellie55@gmail.com>
2021-11-10 11:29:43 +0900

    깃발수 줄어드는 부분까지 함

--- app/src/main/java/com/example/firstapplication/BlockButton.java
+++ app/src/main/java/com/example/firstapplication/BlockButton.java
@@ -1,6 +1,7 @@
 package com.example.firstapplication;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.View;
 
 import androidx.appcompat.widget.AppCompatButton;
@@ -10,14 +11,14 @@ class BlockButton extends AppCompatButton {
     boolean mine;  // 지뢰인지
     boolean flag;  // 깃발인지
     int neighborMines;  //근처 지뢰 갯수
-    static int flags = 0;  //남은 깃발
+    static public int flags = 10;  //남은 깃발
     static int blocks = 81;  //남은 블록
 
-    public BlockButton(Context context, int x, int y) {
+    public BlockButton(Context context, int x, int y, boolean mine) {
         super(context);
         this.x = x;
         this.y = y;
-        this.mine = false;
+        this.mine = mine;
         this.flag = false;
         this.neighborMines = 0;
 //        this.flags = 0;
@@ -27,32 +28,102 @@ class BlockButton extends AppCompatButton {
     public void toggleFlag(){
         if(flag){
             flag = false;
-            flags--;
+            flags++;
             setText("");
         } else {
             flag = true;
-            flags++;
+            flags--;
             setText("+");
         }
     }
 
-    public boolean breakBlock(View view) {
+    public boolean breakBlock() {
         if(flag)  //깃발이 있으면 열지 못함
             return false;
+        if(!isClickable())
+            return false;
         setClickable(false);
+        setBackgroundColor(Color.parseColor("#FFFFFF"));
+
         blocks--;
         if(mine){
-            setText("mine"); // 지뢰 표현
+            setText("*"); // 지뢰 표현
             return true;
         }
         else{
-            setText(neighborMines); //근처 지뢰 수 표현
             //represent the uncovered block
             //여기서 이웃들까지 열어야할 듯
+
             if(neighborMines == 0){
                 //여기서 재귀함수 구현하기..?
+//                MainActivity.buttons[x-1][y-1].breakBlock(); // 대각선 왼쪽 아래
+//                MainActivity.buttons[x+1][y-1].breakBlock(); // 대각선 오른쪽 아래
+//                MainActivity.buttons[x-1][y+1].breakBlock(); // 대각선 왼쪽 위
+//                MainActivity.buttons[x+1][y+1].breakBlock(); // 대각선 오른쪽 위
+//                MainActivity.buttons[x][y-1].breakBlock(); // 아래
+//                MainActivity.buttons[x-1][y].breakBlock(); // 왼쪽
+//                MainActivity.buttons[x][y+1].breakBlock(); // 위
+//                MainActivity.buttons[x+1][y].breakBlock(); // 오른쪽
+
+                if(x>0 && y>0){ // 대각선 왼쪽 아래
+                    MainActivity.buttons[x-1][y-1].breakBlock();
+                } if(x<8 && y<8){ // 대각선 오른쪽 위
+                    MainActivity.buttons[x+1][y+1].breakBlock();
+                } if(x>0 && y<8){ // 대각선 왼쪽 위
+                    MainActivity.buttons[x-1][y+1].breakBlock();
+                } if(y>0 && x<8){ // 대각선 오른쪽 아래
+                    MainActivity.buttons[x+1][y-1].breakBlock();
+                } if(x<8) { // 오른쪽
+                    MainActivity.buttons[x+1][y].breakBlock();
+                } if(x>0){ // 왼쪽
+                    MainActivity.buttons[x-1][y].breakBlock();
+                } if(y<8){ // 위
+                    MainActivity.buttons[x][y+1].breakBlock();
+                } if(y>0){ // 아래
+                    MainActivity.buttons[x][y-1].breakBlock();
+                }
+            }
+            else {
+                setText(String.valueOf(neighborMines)); //근처 지뢰 수 표현
             }
             return false;
         }
+    }
+
+    public void neighborMines(int neighborMines){
+        this.neighborMines = neighborMines;
+    }
+
+    public void open(){
+//        if(!isClickable())
+//            return;
+//        setClickable(false);
+//        setBackgroundColor(Color.parseColor("#FFFFFF"));
+//        setText(String.valueOf(neighborMines)); //근처 지뢰 수 표현
+
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                MainActivity.buttons[x][y].breakBlock();
+            }
+        }
+
+//        if(x>0 && y>0){ // 대각선 왼쪽 아래
+//            MainActivity.buttons[x-1][y-1].open();
+//        } if(x<8 && y<8){ // 대각선 오른쪽 위
+//            MainActivity.buttons[x+1][y+1].open();
+//        } if(x>0 && y<8){ // 대각선 왼쪽 위
+//            MainActivity.buttons[x-1][y+1].open();
+//        } if(y>0 && x<8){ // 대각선 오른쪽 아래
+//            MainActivity.buttons[x+1][y-1].open();
+//        } if(x<8) { // 오른쪽
+//            MainActivity.buttons[x+1][y].open();
+//        } if(x>0){ // 왼쪽
+//            MainActivity.buttons[x-1][y].open();
+//        } if(y<8){ // 위
+//            MainActivity.buttons[x][y+1].open();
+//        } if(y>0){ // 아래
+//            MainActivity.buttons[x][y-1].open();
+//        }
+
     }
 }
--- app/src/main/java/com/example/firstapplication/MainActivity.java
+++ app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -10,36 +10,58 @@ import android.widget.Button;
 import android.widget.Switch;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    static BlockButton[][] buttons = new BlockButton[9][9];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ToggleButton flag = (ToggleButton) findViewById(R.id.toggleButton);
-
+        TextView mineCount = (TextView) findViewById(R.id.MineCount);
         TableLayout table;
         table = (TableLayout)findViewById(R.id.tableLayout);
 
 //        Button[][] buttons = new Button[9][9];
-        BlockButton[][] buttons = new BlockButton[9][9];
 
+        Boolean mines[][] = new Boolean[9][9];
+        for(Boolean[] arr : mines){
+            Arrays.fill(arr, false);
+        }
+
+        Random rand = new Random();
+        for(int i=0; i<10; i++){
+            int x=0, y=0;
+            x = rand.nextInt(9);
+            y = rand.nextInt(9);
+            mines[x][y] = true;
+        }
 
         for(int i=0; i<9; i++){
             TableRow tableRow = new TableRow(this);
 
             for(int j=0; j<9; j++){
 //                buttons[i][j] = new Button(this);
-                buttons[i][j] = new BlockButton(this, i, j);
+                buttons[i][j] = new BlockButton(this, i, j, mines[i][j]);
                 buttons[i][j].setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        if(flag.isChecked())  //토글 버튼이 on이면
-                            ((BlockButton)view).toggleFlag();  //깃발 추가/삭제
+                        if (flag.isChecked()) { //토글 버튼이 on이면
+                            ((BlockButton) view).toggleFlag();  //깃발 추가/삭제
+                            mineCount.setText(String.valueOf(BlockButton.flags));
+                        }
                         else  //토글버튼이 break면
-                            ((BlockButton)view).breakBlock(view);  //칸 열기
+                            if (((BlockButton) view).breakBlock()) { // 칸열기
+//                                ((BlockButton) view).open();
+
+                            }
                     }
                 });
 
@@ -53,6 +75,33 @@ public class MainActivity extends AppCompatActivity {
                 tableRow.addView(buttons[i][j]);
             }
             table.addView(tableRow);
+        }
+
+
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                int neighbMines = 0;
+                if(!mines[i][j]){
+                  if(i>0 && j>0 && mines[i-1][j-1]){ // 대각선 왼쪽 아래
+                      neighbMines++;
+                  } if(i<8 && j<8 && mines[i+1][j+1]){ // 대각선 오른쪽 위
+                      neighbMines++;
+                  } if(i>0 && j<8 && mines[i-1][j+1]){ // 대각선 왼쪽 위
+                      neighbMines++;
+                  } if(j>0 && i<8 && mines[i+1][j-1]){ // 대각선 오른쪽 아래
+                      neighbMines++;
+                  } if(i<8 && mines[i+1][j]){ // 오른쪽
+                      neighbMines++;
+                  } if(i>0 && mines[i-1][j]){ // 왼쪽
+                      neighbMines++;
+                  } if(j<8 && mines[i][j+1]){ // 위
+                      neighbMines++;
+                  } if(j>0 && mines[i][j-1]){ // 아래
+                      neighbMines++;
+                  }
+                }
+                buttons[i][j].neighborMines(neighbMines);
+            }
         }
     }
 }
